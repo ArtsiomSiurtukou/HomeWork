6cd1d0ea0cf02506bdebe242889d6f6dff927ba2
ArtsiomSiurtukou <semiaradosti@mail.ru>
2022-08-18 12:34:49 +0300

    Пользователь вводит 3 числа, найти среднее арифмитическое с точность 3

---
+++ 3.2_own_version.py
@@ -0,0 +1,6 @@
+print('Добро пожаловать в мини-калькулято!')
+number_1 = int(input('Введите 1-е число...'))
+number_2 = int(input('Введите 2-е число...'))
+number_3 = int(input('Введите 3-е число...'))
+result_of_number = number_1 + number_2 + number_3
+print('Результат суммы чисел = ' + str(result_of_number))
