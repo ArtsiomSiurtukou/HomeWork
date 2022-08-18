a2108a3a92572dff7160f9144ecbef2443dc7560
ArtsiomSiurtukou <semiaradosti@mail.ru>
2022-08-18 14:19:48 +0300

    correct

--- 3.2.py
+++ 3.2.py
@@ -1,6 +1,6 @@
-number = input()
+number = input('Введите пожалуйста 3 положительных числа без пробелов, а я их ссумирую - ')
 first_number = int(number[0])
 second_number = int(number[1])
 third_number = int(number[2])
 result = first_number + second_number + third_number
-print(result)
+print('сумма чисел = ' + str(result))
